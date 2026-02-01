ecea7f85e64fa6a998a76d30f74a2354e4053ccc
Shruti270903 <shrutikashyap2709@gmail.com>
2026-02-01 21:34:13 +0530

    Add IF-ELSE programs

---
+++ IF-ELSE/APseries.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;  //4 7 10 13 16
+    cout<<"enter n: ";
+    cin>>n;
+    int a=4, d=3;
+   while(n--){
+ cout<<a<<" ";
+      a+= 3;
+}
+}
