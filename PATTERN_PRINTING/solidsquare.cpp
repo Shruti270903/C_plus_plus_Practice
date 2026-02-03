673d937853a9da32d57fd696f1db57f7c34e1005
Shruti270903 <shrutikashyap2709@gmail.com>
2026-02-03 08:39:12 +0530

    Add PATTERN_PRINTING PROGRAMS

---
+++ PATTERN_PRINTING/solidsquare.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter no. of rows : ";
+    cin>>n;
+    for(int i=1; i<=n ; i++){
+        for(int i=1; i<=n ; i++){
+        cout<<"*";
+    }
+    cout<<"\n";
+}} 
